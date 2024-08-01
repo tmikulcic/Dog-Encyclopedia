f970d286dd9a3da0d4e80af59ff78645de7db9e8
tmikulcic <tom.mikulcic@gmail.com>
2024-08-01 14:00:59 +0200

    improved card layout

--- components/UI/DogCard.tsx
+++ components/UI/DogCard.tsx
@@ -1,20 +1,47 @@
 import { type Dogs } from '@/data/dogs';
 import Link from 'next/link';
-import Image from 'next/image';
 import styles from './DogCard.module.css';
-import imagePortrait from '@/public/images/dog-portrait.jpg';
-import imageLandscape from '@/public/images/dog-landscape.jpg';
 
 type DogCardProps = {
   dog: Dogs;
 };
 
 const DogCard: React.FC<DogCardProps> = ({ dog }) => {
+  let badgeComponent;
+
+  if (dog.size === 'Small') {
+    badgeComponent = (
+      <div className={styles.breedSizeBadge} style={{ backgroundColor: 'green' }}>
+        S
+      </div>
+    );
+  } else if (dog.size === 'Medium') {
+    badgeComponent = (
+      <div className={styles.breedSizeBadge} style={{ backgroundColor: 'gold' }}>
+        M
+      </div>
+    );
+  } else if (dog.size === 'Large') {
+    badgeComponent = (
+      <div className={styles.breedSizeBadge} style={{ backgroundColor: 'purple' }}>
+        L
+      </div>
+    );
+  } else {
+    badgeComponent = (
+      <div className={styles.breedSizeBadge} style={{ backgroundColor: 'red' }}>
+        XL
+      </div>
+    );
+  }
+
   return (
     <li className={styles.card}>
       <Link href={`/${dog.breed}`}>
-        <Image className={styles.cardImage} src={imageLandscape} alt='dog' />
-        <p className={styles.dogBreed}>{dog.breed}</p>
+        <div className={styles.titleBadge}>
+          <p>{dog.breed}</p>
+          {badgeComponent}
+        </div>
         <p className={styles.dogBreedOverview}>{dog.overview}</p>
       </Link>
     </li>
